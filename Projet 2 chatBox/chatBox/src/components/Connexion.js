4ed8a379c841ee79eaeffaf9967476de517411ba
wiliamailysh <victor.douay+dev@gmail.com>
2019-04-22 22:32:07 +0200

    React Router defined in index.js

--- Projet 2 chatBox/chatBox/src/components/Connexion.js
+++ Projet 2 chatBox/chatBox/src/components/Connexion.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
 class Connexion extends React.Component {
+
+  goToChat = (event) => {
+    event.preventDefault();
+    // Récupérer le pseudo
+    const pseudo = this.pseudoInput.value;
+    // Changer l'url
+    this.context.router.transitionTo(`/pseudo/${pseudo}`);
+  };
+
   render() {
     return(
-      <div className="connexionBox">
+      <div className="connexionBox" onSubmit={(e) => this.goToChat(e)}>
         <form className="connexion">
-          <input type="text" placeholder="Votre pseudo" required/>
+          <input
+            type="text"
+            placeholder="Votre pseudo"
+            required
+            ref={(input) => this.pseudoInput = input}
+          />
           <button type="submit">GO</button>
         </form>
       </div>
     )
   }
+
+  static contextTypes = {
+    router: React.PropTypes.object
+  };
 }
 
 export default Connexion;
--- Projet 2 chatBox/chatBox/src/index.js
+++ Projet 2 chatBox/chatBox/src/index.js
@@ -5,12 +5,28 @@ import { render } from 'react-dom';
 // CSS
 import './index.css';
 
+// Rooter
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 // Components
 import Connexion from './components/Connexion';
 import App from './components/App';
+import NotFound from './components/NotFound';
 
+// Component Stateless
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={Connexion} />
+        <Match pattern="/pseudo/:pseudo" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
 
 render(
-  <App />,
+  <Root />,
   document.getElementById('root')
-)
+);
